4a9d8bd37254274f691535f5ad4b69ec474bc043
PeterBrine <pbrine@stevens.edu>
2018-11-04 19:01:44 -0500

    Working test with image. Next step is to fine tune the filter. -PB

--- CPE462_Project.m
+++ CPE462_Project.m
@@ -1,12 +1,31 @@
 filter = [0 0 0 0 0 0 0;
         0 0 0 0 0 0 0;
         0 0 0 0 0 0 0;
-        -0.005 -0.005 -0.005 0.03 -0.005 -0.005 -0.005;
+        -1 -1 -1 6 -1 -1 -1;
         0 0 0 0 0 0 0;
         0 0 0 0 0 0 0;
         0 0 0 0 0 0 0;];
+    
+nullFilter = [0 0 0 0 0 0 0;
+         0 0 0 0 0 0 0;
+         0 0 0 0 0 0 0;
+         0 0 0 1 0 0 0;
+         0 0 0 0 0 0 0;
+         0 0 0 0 0 0 0;
+         0 0 0 0 0 0 0;];
 
-image = imread('boat-test-3.jpg');
-image = rgb2gray(image);
-Y = conv2(image,filter);
-imshow(Y);
+image = rgb2gray(im2double(imread('boat-test-3.jpg')));
+paddedImage = conv2(image,nullFilter);
+output = conv2(image,filter);
+
+subplot(2,2,1);
+imshow(image);
+
+subplot(2,2,2);
+imshow(output);
+
+subplot(2,2,3);
+imshow(output + 0.5);
+
+subplot(2,2,4);
+imshow(abs(output));
